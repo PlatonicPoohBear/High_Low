1017317dfe2c8c8e31db83a0fd1d17db3b45f8a2
PlatonicPoohBear <biologynut10@hotmail.com>
2016-02-29 11:10:48 -0600

    Create highlow.php

--- highlow.php
+++ highlow.php
@@ -1,3 +1,22 @@
 <?php 
 
+$random_number = rand(0, 100);
+
+$input;
+
+do {
+	fwrite(STDOUT, 'Guess? ');
+
+	$input = fgets(STDIN);
+
+	if ($input < $random_number) {
+		echo "HIGHER\n";
+	} elseif ($input > $random_number) {
+		echo "LOWER\n";
+	} elseif ($input == $random_number) {
+		echo "GOOD GUESS\n";
+	}
+
+} while ($input != $random_number)
+
  ?>
